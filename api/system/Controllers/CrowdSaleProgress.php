7990fc8269eb7cf8e5da6a7110f690c8a8b337f7
Marc Godard <me@marcgodard.com>
2016-12-28 10:25:09 -0500

    Clean up

--- api/system/Controllers/CrowdSaleProgress.php
+++ api/system/Controllers/CrowdSaleProgress.php
@@ -16,18 +16,19 @@ class CrowdSaleProgress extends BaseAPIController {
 
             $data = Array(
                 "btcPrices" => Array(
-                    number_format(2*$bitcoinPrice,5),
-                    number_format(2.50*$bitcoinPrice,5),
-                    number_format(3.13*$bitcoinPrice,5),
-                    number_format(3.91*$bitcoinPrice,5),
-                    number_format(4.89*$bitcoinPrice,5),
-                    number_format(6.11*$bitcoinPrice,5),
-                    number_format(7.64*$bitcoinPrice,5),
-                    number_format(9.55*$bitcoinPrice,5),
-                    number_format(11.94*$bitcoinPrice,5),
-                    number_format(14.93*$bitcoinPrice,5)
+                    number_format(2*$bitcoinPrice,6),
+                    number_format(2.50*$bitcoinPrice,6),
+                    number_format(3.13*$bitcoinPrice,6),
+                    number_format(3.91*$bitcoinPrice,6),
+                    number_format(4.89*$bitcoinPrice,6),
+                    number_format(6.11*$bitcoinPrice,6),
+                    number_format(7.64*$bitcoinPrice,6),
+                    number_format(9.55*$bitcoinPrice,6),
+                    number_format(11.94*$bitcoinPrice,6),
+                    number_format(14.93*$bitcoinPrice,6)
                 ),
-                "eqbRemaining" => Array(50000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000)
+                "eqbRemaining" => Array(100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000, 100000),
+                "purchased" => $totalEQBSold
             );
 
             echo json_encode(StatusReturn::S200($data), JSON_NUMERIC_CHECK);
--- site/src/pages-site/page-ico/view-model.js
+++ site/src/pages-site/page-ico/view-model.js
@@ -4,16 +4,7 @@ import 'can/map/define/';
 export default can.Map.extend({
   define: {
     countdown: {
-      value: false,
-      serialize: false
-    },
-    salesStart: {
-      value: 1483228800,
-      serialize: false
-    },
-    salesEnds: {
-      value: 1485907200,
-      serialize: false
+      value: false
     }
   }
 });
